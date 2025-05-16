4415c804fe09393b3b8c46be06a997697af2f19e
Hongyi Wu <hongyi@mac-mini.mynetworksettings.com>
2025-05-15 22:07:53 -0400

    edit key

--- tests/test_optimal_buy_cbpro.py
+++ tests/test_optimal_buy_cbpro.py
@@ -5,8 +5,8 @@ import json
 from coinbase.rest import RESTClient
 
 from optimal_buy_cbpro import optimal_buy_cbpro
-api_key = "organizations/9fb9f9ad-b496-494e-9fdd-40fc978aa72c/apiKeys/4810e4bf-d9dd-41d4-93ed-ea99f1e818ab"
-api_secret = "-----BEGIN EC PRIVATE KEY-----\nMHcCAQEEIB0caGXzcMOsSOENNDwjqYUiZBt11aU24PAXkYUHdFvfoAoGCCqGSM49\nAwEHoUQDQgAEbreKJ/v/rzdSE4aQNwzXbA3elcJDTd8zzj7cSAYTdhToqg5tWNWi\n/6MwkG4zH1oiuH3rhbkZJ7IGMOz5nemQNA==\n-----END EC PRIVATE KEY-----\n"
+api_key = ""
+api_secret = ""
 
 
 
